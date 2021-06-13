263d285ca0ec5a8e512b3b82b87b33f12d9506bb
Matthew <furikuribeebop@gmail.com>
2021-06-13 17:48:07 +0930

    Desktop Layout complete, sass cleaned up

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function dropdown(answer, question) {
     document.getElementById(answer).classList.toggle("hidden");
-    var elements = document.getElementById(question).getElementsByTagName('img');
+    var element = document.getElementById(question);
+    element.classList.toggle("focus");
+    var elements = element.getElementsByTagName('img');
     elements[0].classList.toggle("up");
   }
